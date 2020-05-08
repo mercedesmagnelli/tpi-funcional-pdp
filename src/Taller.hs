5c2281c5b9cff53b717744a6e8bb53cd026d4eb6
Mecha Magnelli <mechamagnelli@gmail.com>
2020-05-08 20:36:22 -0300

    Punto 1 Co-authored-by: motermn <mati_otermin@yahoo.com>

--- src/Taller.hs
+++ src/Taller.hs
@@ -3,6 +3,7 @@ module Taller where
 type Desgaste = Float
 type Patente = String
 type Fecha = (Int, Int, Int)
+type Costo = Int 
  
 -- Definiciones base
 anio :: Fecha -> Int
@@ -15,3 +16,23 @@ data Auto = Auto {
  temperaturaAgua :: Float,
  ultimoArreglo :: Fecha
 } deriving Show
+
+costoDeReparacion :: Auto -> Costo
+costoDeReparacion auto
+ | longitudDeLaPatente auto == 7 = 12500
+ | {- longitudDeLaPatente auto == 6 && -} (patenteEstaEnRango.patente) auto = calculoPatental auto
+ | otherwise = 15000
+
+longitudDeLaPatente :: Auto -> Int
+longitudDeLaPatente auto = (length.patente) auto
+
+patenteEstaEnRango :: Patente -> Bool
+patenteEstaEnRango patente = patente >= "DJ" && patente <= "NB"
+
+calculoPatental :: Auto -> Int
+calculoPatental auto
+ | (terminaEnCuatro.patente) auto = ((*3000).(longitudDeLaPatente)) auto
+ | otherwise = 20000
+
+terminaEnCuatro :: Patente -> Bool
+terminaEnCuatro = ( == '4').(last)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,24 @@
 import Test.Hspec
 import Taller
 
+autoDeAlf = Auto "BC658GB" [0.5,0.1,0,0.2] 1700 90 (07,05,2020)
+autoDeRasta = Auto "JJU564" [0.7,1.3,0.8,0] 1600 90 (23,01,2020)
+autoDeSanti = Auto "HGB475" [0,0.5,0.3,1] 1800 90 (11, 12, 2019)
+autoDeDani = Auto "XSE376" [1, 1.2, 0.7, 0.8] 1900 90 (08,02,2020)
+
+
 main :: IO()
 main = hspec $ do
-   describe "Taller mecanico" $ do
-      it "anio de una fecha" $ do
-         anio (1,1,2020) `shouldBe` 2020
+   describe "Pruebas de costo de reparación" $ do
+      it "El costo de reparación del auto de Alf es $12500" $ do
+         costoDeReparacion autoDeAlf `shouldBe` 12500
+         
+      it "El costo de reparación del auto del Rasta es $18000" $ do
+         costoDeReparacion autoDeRasta `shouldBe` 18000
+         
+      it "El costo de reparacion del auto de Santi es de $20000" $ do
+         costoDeReparacion autoDeSanti `shouldBe` 20000
+         
+      it "El costo de reparacion del auto de Dani es de $15000" $ do
+        costoDeReparacion autoDeDani `shouldBe` 15000         
+
