79d0f4318c4fe7b09261eed9f11678173b5c2a21
Matías Otermín <mati_otermin@yahoo.com>
2020-05-27 17:31:59 -0300

    Punto 7 , parte 1, Matías

--- src/Taller.hs
+++ src/Taller.hs
@@ -1,5 +1,4 @@
 module Taller where
-
 type Desgaste = Float
 type Patente = String
 type Fecha = (Int, Int, Int)
@@ -55,10 +54,11 @@ necesitaRevision = (<= 2015).anio.ultimoArreglo
 
 --- Punto 3 ---
 --- Parte A: Matías ---
+
 data Mecanico = Mecanico {
     nombre :: String,
     reparacion :: Reparacion
-} 
+}
 
 alfa = Mecanico "Alfa" regularRevoluciones
 bravo = Mecanico "Bravo" cambiarCubiertas
@@ -153,8 +153,40 @@ costoTotalDeReparacion :: [Auto] -> Costo
 costoTotalDeReparacion listaAutos = foldl calculoCostoIndividual  0 (filter necesitaRevision listaAutos)
 
 -- Punto 6 -- 
---Parte 2: Mercedes --
+-- Parte 2: Mercedes --
 
 calculoCostoIndividual :: Costo -> Auto -> Costo
 -- calculoCostoIndividual costo auto =  costo + costoDeReparacion auto
 calculoCostoIndividual costo = (+ costo).costoDeReparacion
+
+-- Punto 7 --
+
+-- Parte 1: Matías --
+
+{- Supongamos que generamos una lista infinita a partir de la aplicación de la función cycle a la lista [alfa, bravo, charly, tango, zulu, lima] por 
+ejemplo. Gracias a la estrategia de evaluación perezosa o lazy evaluation que utiliza Haskell, es posible obtener el primer técnico que deja en 
+condiciones a un auto dado, siempre y cuando uno de los técnicos de la lista lo deje en condiciones. De lo contrario Haskell se "colgará".
+
+Por ejemplo, si contamos con la siguiente función y el siguiente auto:
+
+elPrimero :: [a] -> a
+elPrimero lista = lista !! 0
+
+autoDePrueba = Auto "XSE376" [1, 1.2, 0.7, 0.8] 2000 70 (08,02,2019)
+
+Sea la siguiente consulta:
+
+elPrimero (losQueLoDejanEnCondiciones (cycle [alfa, bravo, charly, tango, zulu, lima]) autoDePrueba)
+
+Dicha consulta retornará "Bravo", pues no se necesitará conocer la lista (infinita) completa (ni la de mecánicos ni la de los mecánicos que dejan en 
+condiciones el auto) para saber cuál es el primero que cumple la condición especificada.
+
+En cambio, si por ejemplo tenemos la siguiente consulta:
+
+elPrimero (losQueLoDejanEnCondiciones (cycle [alfa, tango]) autoDePrueba)
+
+Como ni Alfa ni Tango dejan en condiciones al auto en cuestión, entonces va a suceder lo dicho anteriormente: Haskell tratará de encontrar el primer 
+mecánico (de la lista infinita de mecánicos) que deja en condiciones el auto, pero como ninguno de los dos lo hace, la evaluación seguirá realizándose de
+forma indefinida. Algo similar (y más fácil de ver) sucederá si por ejemplo se tratara de aplicar la función any a la lista de números naturales con la 
+condición (< 0).
+-}
